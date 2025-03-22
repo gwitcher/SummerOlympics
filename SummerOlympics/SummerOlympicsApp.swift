c4228887fc8867dcf66d746eda292322ea37a300
Gabe Witcher <gabewitcher@gmail.com>
2025-03-21 17:11:10 -1000

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by Gabe Witcher on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
